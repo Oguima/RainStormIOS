ca2b2663b5d5fa9523fe36f1e489404bf1e84d45
Rafael Guimarães dos Santos <rafael.guimaraes@positivo.com.br>
2019-11-19 12:53:25 -0300

    Initial Commit

---
+++ RainStormTests/RainStormTests.swift
@@ -0,0 +1,34 @@
+//
+//  RainStormTests.swift
+//  RainStormTests
+//
+//  Created by Rafael Guimaraes Dos Santos on 19/11/19.
+//  Copyright © 2019 Guima Games. All rights reserved.
+//
+
+import XCTest
+@testable import RainStorm
+
+class RainStormTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
