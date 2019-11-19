f61b39ea80aa67965474141bc61a4855b4e9a90a
Rafael Guimarães dos Santos <rafael.guimaraes@positivo.com.br>
2019-11-19 13:08:53 -0300

    Project Setup

--- RainStorm/View Controllers/Root View Controller/RootViewController.swift
+++ RainStorm/View Controllers/Root View Controller/RootViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RootViewController.swift
 //  RainStorm
 //
 //  Created by Rafael Guimaraes Dos Santos on 19/11/19.
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class RootViewController: UIViewController {
 
+    // Mark: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
