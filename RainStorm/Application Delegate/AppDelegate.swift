6bd19fda2afea3b6db980a84cb8eb8e1250442f4
Rafael Guimarães dos Santos <rafael.guimaraes@positivo.com.br>
2019-12-02 15:22:01 -0300

    Ajuste SceneDelegate ios 13

--- RainStorm/Application Delegate/AppDelegate.swift
+++ RainStorm/Application Delegate/AppDelegate.swift
@@ -11,16 +11,7 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        guard let rootViewController = window?.rootViewController as? RootViewController else {
-            fatalError("Unexpected Root View Controller")
-        }
-        
-        let rootViewModel = RootViewModel()
-        rootViewController.viewModel = rootViewModel
         
         return true
     }
